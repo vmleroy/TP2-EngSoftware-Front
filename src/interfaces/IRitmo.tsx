3777a22a0164876c8161c91f67f338abe22794fb
Julio Cesar Rocha <54746730+julioCROS@users.noreply.github.com>
2022-06-23 20:22:57 -0300

    Delete IRitmo.tsx

--- src/interfaces/IRitmo.tsx
+++
@@ -1,9 +0,0 @@
-export default interface IRitmo {
-    _id: string,
-    dia: [],
-    aulaInicio: Date,
-    aulaFim: Date,
-    maxAlunos: number,
-    alunosAtivos: number,
-    taCheia: boolean
-}
