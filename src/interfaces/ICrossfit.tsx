8b4cfa3621acf53e9e50525ba1f2c05cae18f3e2
Julio Cesar Rocha <54746730+julioCROS@users.noreply.github.com>
2022-06-23 20:24:15 -0300

    Delete ICrossfit.tsx

--- src/interfaces/ICrossfit.tsx
+++
@@ -1,9 +0,0 @@
-export default interface ICrossfit {
-    _id: string,
-    dia: [],
-    aulaInicio: Date,
-    aulaFim: Date,
-    maxAlunos: number,
-    alunosAtivos: number,
-    taCheia: boolean
-}
